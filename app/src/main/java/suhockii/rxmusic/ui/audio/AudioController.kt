600dbed477a37e2c3cf6ee6510b08337a9a31872
Maksim Sukhotski <suhockii@gmail.com>
2017-04-11 01:16:39 +0300

    Fixed RecyclerView after rotation

--- app/src/main/java/suhockii/rxmusic/ui/audio/AudioController.kt
+++ app/src/main/java/suhockii/rxmusic/ui/audio/AudioController.kt
@@ -9,34 +9,22 @@ import com.bluelinelabs.conductor.RouterTransaction
 import com.bluelinelabs.conductor.changehandler.HorizontalChangeHandler
 import kotlinx.android.synthetic.main.controller_audio.view.*
 import suhockii.rxmusic.R
+import suhockii.rxmusic.data.repositories.audio.models.Audio
 import suhockii.rxmusic.data.repositories.audio.models.AudioResponse
 import suhockii.rxmusic.extension.InfiniteScrollListener
 import suhockii.rxmusic.ui.base.MoxyController
 import suhockii.rxmusic.ui.login.LoginController
 
+
 /** Created by Maksim Sukhotski on 4/8/2017. */
 class AudioController : MoxyController(), AudioView {
-
-    override fun onViewBound(view: View) {
-        with(view) {
-            presenter.validateCredentials()
-            audioRecyclerView.layoutManager = linearLayoutManager
-            adapter = AudioAdapter(kotlin.collections.arrayListOf(), { presenter.playAudio(it) })
-            audioRecyclerView.adapter = adapter
-            audioRecyclerView.addOnScrollListener(
-                    InfiniteScrollListener({
-                        offset += 30
-                        presenter.getAudio(offset = offset.toString())
-                    }, linearLayoutManager)
-            )
-        }
-    }
 
     @InjectPresenter
     lateinit var presenter: AudioPresenter
 
-    private var adapter: AudioAdapter = AudioAdapter()
-    var offset: Int = 0
+    private var offset: Int = 0
+    private var items: MutableList<Audio> = arrayListOf()
+    private var adapter: AudioAdapter = AudioAdapter(items, { presenter.playAudio(it) })
 
     override fun getTitle(): String = "AudioController"
 
@@ -44,12 +32,26 @@ class AudioController : MoxyController(), AudioView {
         return inflater.inflate(R.layout.controller_audio, container, false)
     }
 
+    override fun onViewBound(view: View) {
+        with(view) {
+            presenter.validateCredentials()
+            val linearLayoutManager = LinearLayoutManager(activity)
+            audioRecyclerView.setHasFixedSize(true)
+            audioRecyclerView.layoutManager = linearLayoutManager
+            audioRecyclerView.adapter = adapter
+            audioRecyclerView.addOnScrollListener(InfiniteScrollListener({
+                offset += 30
+                presenter.getAudio(offset = offset.toString())
+            }, linearLayoutManager))
+        }
+    }
+
     override fun showAudio(audioResponse: AudioResponse) {
+        items.addAll(audioResponse.items)
         adapter.items.addAll(audioResponse.items)
         adapter.notifyDataSetChanged()
     }
 
-    private val linearLayoutManager = LinearLayoutManager(activity)
 
     override fun showLoginController() {
         router.setRoot(RouterTransaction.with(LoginController())
